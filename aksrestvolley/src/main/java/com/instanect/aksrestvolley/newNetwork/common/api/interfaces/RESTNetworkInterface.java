0089bcf62681bfb33e6854775504987910c5643e
AKS <ashvinisaxena77@gmail.com>
2018-03-29 14:45:39 +0530

    Extended REST API

--- aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/common/api/interfaces/RESTNetworkInterface.java
+++ aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/common/api/interfaces/RESTNetworkInterface.java
@@ -2,13 +2,15 @@ package com.instanect.aksrestvolley.newNetwork.common.api.interfaces;
 
 import android.net.Uri;
 
+import com.instanect.networkcommon.NetworkInterface;
+
 import java.util.HashMap;
 
 /**
  * Created by AKS on 10/5/2017.
  */
 
-public interface RESTNetworkInterface {
+public interface RESTNetworkInterface extends NetworkInterface{
 
     void execute(
             int requestId,
