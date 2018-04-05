509a93fa213f80fc37256b2bb0021daa943da3f4
AKS <ashvinisaxena77@gmail.com>
2018-04-05 17:11:08 +0530

    Adding more helpers

--- aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/di/NetworkService.java
+++ aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/di/NetworkService.java
@@ -1,15 +1,28 @@
 package com.instanect.aksrestvolley.newNetwork.di;
 
+import com.instanect.aksrestvolley.newNetwork.common.scenario.executor.interfaces.ScenarioExecutionResponseInterface;
+import com.instanect.aksrestvolley.newNetwork.common.scenario.scenario.builder.ScenarioBuilder;
+import com.instanect.aksrestvolley.newNetwork.common.scenario.scenario.interfaces.ScenarioInterface;
 import com.instanect.aksrestvolley.newNetwork.common.scenario.scenario.service.ScenarioService;
+import com.instanect.networkcommon.NetworkInterface;
 
-public class NetworkService {
+public class NetworkService implements NetworkInterface {
 
     private final ScenarioService scenarioService;
 
     public NetworkService(ScenarioService scenarioService) {
 
-
         this.scenarioService = scenarioService;
     }
 
+
+    public ScenarioBuilder getBuilder() {
+        return scenarioService.getScenarioBuilder();
+    }
+
+    public void execute(ScenarioInterface scenarioInterface, ScenarioExecutionResponseInterface
+            scenarioExecutionResponseInterface) {
+
+        scenarioService.execute(scenarioInterface, scenarioExecutionResponseInterface);
+    }
 }
