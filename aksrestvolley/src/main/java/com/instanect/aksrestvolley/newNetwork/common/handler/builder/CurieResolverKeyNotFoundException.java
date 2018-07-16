71c4c7f74ede84517425972b69964cf8afaee5ad
Ashvini Kumar Saxena <ashvinisaxena77@gmail.com>
2018-07-16 16:58:48 +0530

    Problem with null url on key value not found resolved However that it should inform that caller is not resolved

--- aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/common/handler/builder/CurieResolverKeyNotFoundException.java
+++ aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/common/handler/builder/CurieResolverKeyNotFoundException.java
@@ -2,7 +2,7 @@ package com.instanect.aksrestvolley.newNetwork.common.handler.builder;
 
 public class CurieResolverKeyNotFoundException extends Exception {
 
-    CurieResolverKeyNotFoundException(String key) {
+    public CurieResolverKeyNotFoundException(String key) {
 
         super(key + "not found in the result value ");
     }
