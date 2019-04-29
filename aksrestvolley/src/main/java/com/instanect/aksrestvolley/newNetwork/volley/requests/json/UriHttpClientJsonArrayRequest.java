c0a1a8c38953a804aad3afa81c900836ee790fe4
Ashvini Kumar Saxena <ashvinisaxena77@gmail.com>
2019-04-29 16:02:42 +0530

    Merging branch with tags

--- aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/volley/requests/json/UriHttpClientJsonArrayRequest.java
+++ aksrestvolley/src/main/java/com/instanect/aksrestvolley/newNetwork/volley/requests/json/UriHttpClientJsonArrayRequest.java
@@ -3,16 +3,12 @@ package com.instanect.aksrestvolley.newNetwork.volley.requests.json;
 import android.content.Context;
 import android.util.Log;
 
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
