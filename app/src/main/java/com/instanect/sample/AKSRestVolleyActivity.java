89642888d286d7436711084ad94841bdf5f30ae9
Ashvini Kumar Saxena <ashvinisaxena77@gmail.com>
2019-05-20 19:56:56 +0530

    Android x 0.1.0-alpha-01

--- app/src/main/java/com/instanect/sample/AKSRestVolleyActivity.java
+++ app/src/main/java/com/instanect/sample/AKSRestVolleyActivity.java
@@ -1,8 +1,9 @@
 package com.instanect.sample;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.instanect.aksrestvolley.newNetwork.di.NetworkService;
 import com.instanect.sample.application.MainApplication;
